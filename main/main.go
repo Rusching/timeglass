e1e8362c57e8a9ddbe9e0bd8c12520f62632fe39
Rusching <2785442070@qq.com>
2024-02-04 22:09:42 -0600

    [dev]add reset, add border

--- main/main.go
+++ main/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"golang.org/x/term"
@@ -17,24 +18,26 @@ const (
 )
 
 type Glass struct {
-	upper      [][]bool
-	lower      [][]bool
-	upperState []bool
-	lowerState []bool
-	screenH    int
-	screenW    int
+	upper       [][]bool
+	lower       [][]bool
+	upperState  []bool
+	lowerState  []bool
+	screenH     int
+	screenW     int
+	totalHeight int
 }
 
 func NewGlass() *Glass {
 	width, height, _ := term.GetSize(int(os.Stdout.Fd()))
 
 	g := &Glass{
-		upper:      make([][]bool, Height),
-		lower:      make([][]bool, Height),
-		upperState: make([]bool, Height),
-		lowerState: make([]bool, Height),
-		screenW:    width,
-		screenH:    height,
+		upper:       make([][]bool, Height),
+		lower:       make([][]bool, Height),
+		upperState:  make([]bool, Height),
+		lowerState:  make([]bool, Height),
+		screenW:     width,
+		screenH:     height,
+		totalHeight: Width + 4,
 	}
 
 	for i := range g.upperState {
@@ -53,7 +56,26 @@ func NewGlass() *Glass {
 	return g
 }
 
-func (g *Glass) SandFlow() {
+func (g *Glass) reset() {
+	for i := range g.upperState {
+		g.upperState[i] = true
+		g.lowerState[i] = false
+	}
+
+	for i := range g.upper {
+		for j := Height; j <= Height+i; j++ {
+			g.upper[i][j] = true
+			g.upper[i][Width-j-1] = true
+		}
+	}
+	for i := range g.lower {
+		for j := range g.lower[i] {
+			g.lower[i][j] = false
+		}
+	}
+}
+
+func (g *Glass) sandFlow() {
 	upperIdx := -1
 	// for upper:
 	// true   ->  has sand
@@ -100,62 +122,69 @@ func (g *Glass) SandFlow() {
 			break
 		}
 	}
+	if g.lowerState[0] {
+		g.reset()
+	}
 }
 
 func (g *Glass) PrintGlass() {
-	totalHeight := Width
-	startY := (g.screenH - totalHeight) / 2
-	startX := (g.screenW - Width) / 2
+	startY := (g.screenH - g.totalHeight) / 2
+	startX := (g.screenW - Width - 2*GlassWidth) / 2
+	fmt.Printf("\033[%d;%dH%s\n", startY, startX, strings.Repeat("=", Width+2*GlassWidth))
 	var out bytes.Buffer
 	for i := Height - 1; i >= 0; i-- {
-		for j := 0; j < Width; j++ {
+		for k := 0; k < Height-i-1; k++ {
+			out.WriteRune(' ')
+		}
+		out.WriteRune('\\')
+		out.WriteRune('\\')
+		s := Height - i - 1
+		for j := s; j <= Width-s-1; j++ {
 			if g.upper[i][j] {
 				out.WriteRune('*')
 			} else {
 				out.WriteRune(' ')
 			}
 		}
+		out.WriteRune('/')
+		out.WriteRune('/')
 		out.WriteRune('\n')
-		fmt.Printf("\033[%d;%dH%s\n", startY+Height-i-1, startX, out.String())
+		fmt.Printf("\033[%d;%dH%s\n", startY+Height-i, startX, out.String())
 		out.Reset()
 	}
+
+	fmt.Printf("\033[%d;%dH%s\n", startY+Height+1, startX, strings.Repeat(" ", Height)+"\\\\//")
+	fmt.Printf("\033[%d;%dH%s\n", startY+Height+2, startX, strings.Repeat(" ", Height)+"//\\\\")
+
 	for i := 0; i < Height; i++ {
-		for j := 0; j < Width; j++ {
+		for k := 0; k < Height-i-1; k++ {
+			out.WriteRune(' ')
+		}
+		out.WriteRune('/')
+		out.WriteRune('/')
+		s := Height - i - 1
+		for j := s; j <= Width-s-1; j++ {
 			if g.lower[i][j] {
 				out.WriteRune('*')
 			} else {
 				out.WriteRune(' ')
 			}
 		}
+		out.WriteRune('\\')
+		out.WriteRune('\\')
 		out.WriteRune('\n')
-		fmt.Printf("\033[%d;%dH%s\n", startY+Height+i, startX, out.String())
+		fmt.Printf("\033[%d;%dH%s\n", startY+Height+i+3, startX, out.String())
 		out.Reset()
 	}
-}
+	fmt.Printf("\033[%d;%dH%s\n", startY+Width+3, startX, strings.Repeat("=", Width+2*GlassWidth))
+	now := time.Now()
 
-func getScreenSize() {
-	w, h, _ := term.GetSize(int(os.Stdout.Fd()))
-	content := "Hello, World!"
-
-	// 计算开始打印的位置
-	startX := (w - len(content)) / 2
-	startY := h / 2
-
-	// 移动光标到指定位置
-	fmt.Printf("\033[%d;%dH", startY, startX)
-
-	// 打印内容
-	fmt.Println(content)
+	fmt.Printf("\033[%d;%dH%s\n", startY+Width+5, (g.screenW-18)/2, now.Format("2006-01-02 15:04:05"))
 }
 
 func main() {
 	g := NewGlass()
-	// for {
-	// 	fmt.Print("\033[2J\033[3J\033[H")
-	// 	getScreenSize()
-	// }
-
-	for i := 0; i <= TotalSands; i++ {
+	for {
 		// 清除屏幕并移动光标到左上角
 		// fmt.Print("\033[2J\033[H")
 		fmt.Print("\033[2J\033[3J\033[H")
@@ -165,6 +194,6 @@ func main() {
 		g.PrintGlass()
 		// 等待一段时间
 		time.Sleep(time.Second / 5)
-		g.SandFlow()
+		g.sandFlow()
 	}
 }
